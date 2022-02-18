fc3e15e4d8a00f8de3f4c7f4f5f35667b0a8ea53
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 23:35:23 +0700

    use eslint

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,8 +1,8 @@
-const Hapi = require("@hapi/hapi");
-const ClientError = require("../../Commons/exceptions/ClientError");
-const DomainErrorTranslator = require("../../Commons/exceptions/DomainErrorTranslator");
-const users = require("../../Interfaces/http/api/users");
-const authentications = require("../../Interfaces/http/api/authentications");
+const Hapi = require('@hapi/hapi');
+const ClientError = require('../../Commons/exceptions/ClientError');
+const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
+const users = require('../../Interfaces/http/api/users');
+const authentications = require('../../Interfaces/http/api/authentications');
 
 const createServer = async (container) => {
   const server = Hapi.server({
@@ -23,14 +23,15 @@ const createServer = async (container) => {
 
   server.route(
     {
-      method: "GET",
-      path: "/",
+      method: 'GET',
+      path: '/',
       handler: () => ({
-        value: "Hello world!",
+        value: 'Hello world!',
       }),
-    });
+    },
+  );
 
-  server.ext("onPreResponse", (request, h) => {
+  server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
 
@@ -41,7 +42,7 @@ const createServer = async (container) => {
       // penanganan client error secara internal.
       if (translatedError instanceof ClientError) {
         const newResponse = h.response({
-          status: "fail",
+          status: 'fail',
           message: translatedError.message,
         });
         newResponse.code(translatedError.statusCode);
@@ -55,8 +56,8 @@ const createServer = async (container) => {
 
       // penanganan server error sesuai kebutuhan
       const newResponse = h.response({
-        status: "error",
-        message: "terjadi kegagalan pada server kami",
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami',
       });
       newResponse.code(500);
       return newResponse;
