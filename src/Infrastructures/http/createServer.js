18c45761310f3b4234b34b986dcd380e506f1710
Moh Amilin <amilinmoh99@gmail.com>
2022-02-18 22:37:48 +0700

    say hello

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -21,13 +21,7 @@ const createServer = async (container) => {
     },
   ]);
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: () => ({
-      value: "Hello world!",
-    }),
-  });
+
 
   server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
