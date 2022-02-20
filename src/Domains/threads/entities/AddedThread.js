74d0018ec2d9303117ffc9c21f7aea85445c5d36
mohamilin <amilinmoh99@gmail.com>
2022-02-20 22:06:24 +0700

    add AddedThread

---
+++ src/Domains/threads/entities/AddedThread.js
@@ -0,0 +1,25 @@
+class AddedThread {
+  constructor(payload) {
+    this._verifyPayload(payload);
+    const { id, title, owner } = payload;
+    this.id = id;
+    this.title = title;
+    this.owner = owner;
+  }
+
+  _verifyPayload({ id, title, owner }) {
+    if (!id || !title || !owner) {
+      throw new Error("THREAD.NOT_CONTAIN_NEEDED");
+    }
+
+    if (
+      typeof id !== "string" ||
+      typeof title !== "string" ||
+      typeof owner !== "string"
+    ) {
+      throw new Error("THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION");
+    }
+  }
+}
+
+module.exports = AddedThread;
