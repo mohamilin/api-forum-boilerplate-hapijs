633f1f806cf7534fba3d65e9f5a097dd420541c1
mohamilin <amilinmoh99@gmail.com>
2022-02-20 22:45:21 +0700

    Register comment plugin

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -5,6 +5,7 @@ const DomainErrorTranslator = require("../../Commons/exceptions/DomainErrorTrans
 const users = require("../../Interfaces/http/api/users");
 const authentications = require("../../Interfaces/http/api/authentications");
 const threads = require("../../Interfaces/http/api/threads");
+const comments = require("../../Interfaces/http/api/comments");
 
 const createServer = async (container) => {
   const server = Hapi.server({
@@ -46,6 +47,10 @@ const createServer = async (container) => {
     },
     {
       plugin: threads,
+      options: { container },
+    },
+    {
+      plugin: comments,
       options: { container },
     },
   ]);
