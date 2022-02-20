e554380a1f008dc6b4cdc3f0f76bd56aadf95790
mohamilin <amilinmoh99@gmail.com>
2022-02-20 22:06:01 +0700

    add NewThread

---
+++ src/Domains/threads/entities/NewThread.js
@@ -0,0 +1,25 @@
+class NewThread {
+  constructor(payload) {
+    this._verifyPayload(payload);
+    const { owner, title, body } = payload;
+    this.owner = owner;
+    this.title = title;
+    this.body = body;
+  }
+
+  _verifyPayload({ owner, title, body }) {
+    if (!owner || !title || !body) {
+      throw new Error("THREAD.NOT_CONTAIN_NEEDED");
+    }
+
+    if (
+      typeof owner !== "string" ||
+      typeof title !== "string" ||
+      typeof body !== "string"
+    ) {
+      throw new Error("THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION");
+    }
+  }
+}
+
+module.exports = NewThread;
