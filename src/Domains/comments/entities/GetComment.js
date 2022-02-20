5dc09e66871b0e40b51fb35a304add760dcef614
mohamilin <amilinmoh99@gmail.com>
2022-02-20 22:43:44 +0700

    add entities for GetComment

---
+++ src/Domains/comments/entities/GetComment.js
@@ -0,0 +1,33 @@
+class GetComment {
+  constructor(payload) {
+    this._verifyPayload(payload);
+    const {
+      id, username, content, date, is_delete,
+    } = payload;
+    this.id = id;
+    this.username = username;
+    this.content = content;
+    this.date = date;
+    this.is_delete = is_delete;
+  }
+
+  _verifyPayload({
+    id, date, username, content, is_delete,
+  }) {
+    if (!id || !date || !username || !content) {
+      throw new Error('GET_COMMENT.NOT_CONTAIN_NEEDED');
+    }
+
+    if (
+      typeof id !== 'string'
+      || typeof date !== 'string'
+      || typeof username !== 'string'
+      || typeof content !== 'string'
+      || typeof is_delete !== 'boolean'
+    ) {
+      throw new Error('GET_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    }
+  }
+}
+
+module.exports = GetComment;
