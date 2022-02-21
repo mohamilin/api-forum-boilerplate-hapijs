35279b22b469f36a7d540576d4f7896ff02163cb
mohamilin <amilinmoh99@gmail.com>
2022-02-21 12:22:51 +0700

    add exception error for add comment

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -1,65 +1,65 @@
-const DomainErrorTranslator = require("../DomainErrorTranslator");
-const InvariantError = require("../InvariantError");
+const DomainErrorTranslator = require('../DomainErrorTranslator');
+const InvariantError = require('../InvariantError');
 
-describe("DomainErrorTranslator", () => {
-  it("should translate error correctly", () => {
+describe('DomainErrorTranslator', () => {
+  it('should translate error correctly', () => {
     expect(
       DomainErrorTranslator.translate(
-        new Error("REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY")
-      )
+        new Error('REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY'),
+      ),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada"
-      )
+        'tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada',
+      ),
     );
     expect(
       DomainErrorTranslator.translate(
-        new Error("REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION")
-      )
+        new Error('REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION'),
+      ),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat user baru karena tipe data tidak sesuai"
-      )
+        'tidak dapat membuat user baru karena tipe data tidak sesuai',
+      ),
     );
     expect(
       DomainErrorTranslator.translate(
-        new Error("REGISTER_USER.USERNAME_LIMIT_CHAR")
-      )
+        new Error('REGISTER_USER.USERNAME_LIMIT_CHAR'),
+      ),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat user baru karena karakter username melebihi batas limit"
-      )
+        'tidak dapat membuat user baru karena karakter username melebihi batas limit',
+      ),
     );
     expect(
       DomainErrorTranslator.translate(
-        new Error("REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER")
-      )
+        new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER'),
+      ),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat user baru karena username mengandung karakter terlarang"
-      )
+        'tidak dapat membuat user baru karena username mengandung karakter terlarang',
+      ),
     );
     expect(
-      DomainErrorTranslator.translate(new Error("THREAD.NOT_CONTAIN_NEEDED"))
+      DomainErrorTranslator.translate(new Error('THREAD.NOT_CONTAIN_NEEDED')),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada"
-      )
+        'tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada',
+      ),
     );
     expect(
       DomainErrorTranslator.translate(
-        new Error("THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION")
-      )
+        new Error('THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION'),
+      ),
     ).toStrictEqual(
       new InvariantError(
-        "tidak dapat membuat thread baru karena tipe data tidak sesuai"
-      )
+        'tidak dapat membuat thread baru karena tipe data tidak sesuai',
+      ),
     );
   });
 
-  it("should return original error when error message is not needed to translate", () => {
+  it('should return original error when error message is not needed to translate', () => {
     // Arrange
-    const error = new Error("some_error_message");
+    const error = new Error('some_error_message');
 
     // Action
     const translatedError = DomainErrorTranslator.translate(error);
