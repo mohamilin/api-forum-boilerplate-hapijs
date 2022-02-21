1bc774da342fa4f344efa918588ac8eb47d1380c
mohamilin <amilinmoh99@gmail.com>
2022-02-21 12:28:15 +0700

    add function getCommentWithThread

--- src/Infrastructures/repository/CommentRepositoryPostgres.js
+++ src/Infrastructures/repository/CommentRepositoryPostgres.js
@@ -1,9 +1,9 @@
-const CommentRepository = require("../../Domains/comments/CommentRepository");
-const AddedComment = require("../../Domains/comments/entities/AddedComment");
-const GetComment = require("../../Domains/comments/entities/GetComment");
+const CommentRepository = require('../../Domains/comments/CommentRepository');
+const AddedComment = require('../../Domains/comments/entities/AddedComment');
+const GetComment = require('../../Domains/comments/entities/GetComment');
 
-const NotFoundError = require("../../Commons/exceptions/NotFoundError");
-const AuthorizationError = require("../../Commons/exceptions/AuthorizationError");
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const AuthorizationError = require('../../Commons/exceptions/AuthorizationError');
 
 class CommentRepositoryPostgres extends CommentRepository {
   constructor(pool, idGenerator) {
@@ -16,7 +16,7 @@ class CommentRepositoryPostgres extends CommentRepository {
     const id = `comment-${this._idGenerator()}`;
     const date = new Date().toISOString();
     const query = {
-      text: "INSERT INTO comments VALUES($1, $2, $3, $4, $5) RETURNING id, content, thread_id, owner",
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5) RETURNING id, content, thread_id, owner',
       values: [
         id,
         newComment.content,
@@ -36,7 +36,7 @@ class CommentRepositoryPostgres extends CommentRepository {
 
   async verifyAvailableComment(commentId) {
     const query = {
-      text: "SELECT id FROM comments WHERE ID = $1",
+      text: 'SELECT id FROM comments WHERE ID = $1',
       values: [commentId],
     };
     const result = await this._pool.query(query);
@@ -47,37 +47,37 @@ class CommentRepositoryPostgres extends CommentRepository {
 
   async verifyOwnerComment({ owner, commentId }) {
     const query = {
-      text: "SELECT id from comments WHERE owner = $1 AND id = $2",
+      text: 'SELECT id from comments WHERE owner = $1 AND id = $2',
       values: [owner, commentId],
     };
 
     const result = await this._pool.query(query);
     if (!result.rowCount) {
-      throw new AuthorizationError("Komentar bukan milik anda");
+      throw new AuthorizationError('Komentar bukan milik anda');
     }
   }
 
   async deleteComment({ commentId }) {
     const query = {
-      text: "UPDATE comments SET is_delete = true WHERE id = $1",
+      text: 'UPDATE comments SET is_delete = true WHERE id = $1',
       values: [commentId],
     };
 
     await this._pool.query(query);
   }
 
-  // async getCommentWithThread(threadId) {
-  //   const query = {
-  //     text: `SELECT comments.*, users.username
-  //     FROM comments LEFT JOIN users ON users.id = comments.owner
-  //     WHERE comments.thread_id = $1
-  //     ORDER BY date ASC`,
-  //     values: [threadId],
-  //   };
+  async getCommentWithThread(threadId) {
+    const query = {
+      text: `SELECT comments.*, users.username
+      FROM comments LEFT JOIN users ON users.id = comments.owner
+      WHERE comments.thread_id = $1
+      ORDER BY date ASC`,
+      values: [threadId],
+    };
 
-  //   const result = await this._pool.query(query);
-  //   return result.rows.map((item) => new GetComment(item));
-  // }
+    const result = await this._pool.query(query);
+    return result.rows.map((item) => new GetComment(item));
+  }
 }
 
 module.exports = CommentRepositoryPostgres;
