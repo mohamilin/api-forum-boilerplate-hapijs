30238a8fba8a4abfa98458eef374b9e0c94b7fec
mohamilin <amilinmoh99@gmail.com>
2022-02-21 12:21:01 +0700

    add : table comments

--- migrations/1642884900984_create-table-threads.js
+++ migrations/1642884900984_create-table-threads.js
@@ -1,36 +1,35 @@
 exports.up = (pgm) => {
-    pgm.createTable("threads", {
-      id: {
-        type: "VARCHAR(50)",
-        primaryKey: true,
-      },
-      title: {
-        type: "TEXT",
-        notNull: true,
-      },
-      body: {
-        type: "TEXT",
-        notNull: true,
-      },
-      owner: {
-        type: "VARCHAR(50)",
-        notNull: true,
-      },
-      date: {
-        type: 'timestamp',
-        notNull: true,
-        default: pgm.func('current_timestamp')
-        }
-    });
-  
-    pgm.addConstraint(
-      "threads",
-      "fk_threads.owner_users.id",
-      "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE"
-    );
-  };
-  
-  exports.down = (pgm) => {
-    pgm.dropTable("threads");
-  };
-  
+  pgm.createTable("threads", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "TEXT",
+      notNull: true,
+    },
+    body: {
+      type: "TEXT",
+      notNull: true,
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    date: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+
+  pgm.addConstraint(
+    "threads",
+    "fk_threads.owner_users.id",
+    "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE"
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("threads");
+};
