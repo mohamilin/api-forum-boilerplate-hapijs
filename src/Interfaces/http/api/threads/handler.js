07146c5d396808ef1808f3a8e66d921755efac4f
mohamilin <amilinmoh99@gmail.com>
2022-02-21 12:29:15 +0700

    add getDetailThread

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,10 +1,12 @@
-const AddThreadUseCase = require("../../../../Applications/use_case/threads/AddThreadUseCase");
+const AddThreadUseCase = require('../../../../Applications/use_case/threads/AddThreadUseCase');
+const GetDetailThreadUsecase = require('../../../../Applications/use_case/threads/GetDetailThreadUsecase');
 
 class ThreadHandler {
   constructor(container) {
     this._container = container;
 
     this.addThread = this.addThread.bind(this);
+    this.getDetailThread = this.getDetailThread.bind(this);
   }
 
   async addThread(request, h) {
@@ -15,12 +17,28 @@ class ThreadHandler {
     const addedThread = await postThread.execute(useCasePayload, credential);
 
     const response = h.response({
-      status: "success",
+      status: 'success',
       data: {
         addedThread,
       },
     });
     response.code(201);
+    return response;
+  }
+
+  async getDetailThread(request, h) {
+    const { threadId } = request.params;
+    const getDetailThread = this._container.getInstance(
+      GetDetailThreadUsecase.name,
+    );
+    const thread = await getDetailThread.execute(threadId);
+    const response = h.response({
+      status: 'success',
+      data: {
+        thread,
+      },
+    });
+    response.code(200);
     return response;
   }
 }
--- src/Interfaces/http/api/threads/index.js
+++ src/Interfaces/http/api/threads/index.js
@@ -1,8 +1,8 @@
-const ThreadHandler = require("./handler");
-const routes = require("./routes");
+const ThreadHandler = require('./handler');
+const routes = require('./routes');
 
 module.exports = {
-  name: "threads",
+  name: 'threads',
   register: async (server, { container }) => {
     const threadHandler = new ThreadHandler(container);
     server.route(routes(threadHandler));
