8528dfc9408afecfa5d5066fb2105232cfd54b0c
mohamilin <amilinmoh99@gmail.com>
2022-02-21 12:23:40 +0700

    add getThreadById function and use eslint

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -1,10 +1,14 @@
 class ThreadRepository {
   async addThread(payload) {
-    throw new Error("THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
   async verifyAvailableThread(threadId) {
-    throw new Error("THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getThreadById(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 }
 
--- src/Domains/threads/_test/ThreadRepository.test.js
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -1,15 +1,18 @@
-const ThreadRepository = require("../ThreadRepository");
+const ThreadRepository = require('../ThreadRepository');
 
-describe("ThreadRepository domains interface", () => {
-  it("should throw error when invoke abstract behavior", async () => {
+describe('ThreadRepository domains interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
     const threadRepository = new ThreadRepository();
 
     // Action and Assert
-    await expect(threadRepository.addThread("")).rejects.toThrowError(
-      "THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED"
+    await expect(threadRepository.addThread('')).rejects.toThrowError(
+      'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
     );
     await expect(
-      threadRepository.verifyAvailableThread("")
-    ).rejects.toThrowError("THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED");
+      threadRepository.verifyAvailableThread(''),
+    ).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(threadRepository.getThreadById('')).rejects.toThrowError(
+      'THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED',
+    );
   });
 });
